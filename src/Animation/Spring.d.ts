6dc42446b27f6f561b20e995840e328ff939dbca
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-08-21 18:06:11 +0200

    disable optional feature types

--- src/Animation/Spring.d.ts
+++ src/Animation/Spring.d.ts
@@ -4,10 +4,12 @@ export declare interface Spring<T> {
 	type: "State";
 	kind: "Spring";
 	get(asDependency?: boolean): T;
+	/*
 	setDamping(damping: number): void;
 	setSpeed(damping: number): void;
 	setPosition(position: T): void;
 	setVelocity(velocity: T): void;
 	addVelocity(velocity: T): void;
+	*/
 }
 export declare function Spring<T extends Animatable>(goalState: State<T>, speed?: number, damping?: number): Spring<T>;
