62e990f3df5d7dbdd2e9a969cc168fcf3b145bda
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-10-08 21:17:50 +0200

    Remove JSX

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,14 @@
-/// <reference path="./jsx.d.ts" />
-
 import { Spring } from "./Animation/Spring";
 import { Tween } from "./Animation/Tween";
-import { ChildrenValue } from "./Instances/New";
+import { Children } from "./Instances/Children";
+import { ChildrenValue, New, NewProperties } from "./Instances/New";
+import { OnChange } from "./Instances/OnChange";
+import { OnEvent } from "./Instances/OnEvent";
 import { Compat } from "./State/Compat";
 import { Computed } from "./State/Computed";
 import { ComputedPairs } from "./State/ComputedPairs";
 import { State } from "./State/State";
-import { Animatable, Error } from "./Types";
+import { Animatable, Error, StateOrValue } from "./Types";
 
 // Fusion Lua
 declare namespace Fusion {
@@ -15,34 +16,10 @@ declare namespace Fusion {
 	export { Compat, Computed, ComputedPairs, State };
 	// Animation
 	export { Spring, Tween };
-	// Util types
-	export { Animatable, ChildrenValue, Error };
-}
-
-// JSX Shims
-declare namespace Fusion {
-	type Element = Instance;
-	type Fragment = Array<Instance>;
-	export function createFragment(elements?: { [elementName: string]: Instance }): Fragment;
-	export function createFragment(elements?: ReadonlyMap<string | number, Instance>): Fragment;
-	export function createFragment(elements?: ReadonlyArray<Instance>): Fragment;
-	export function createElement<T extends keyof CreatableInstances, I extends CreatableInstances[T]>(
-		component: T,
-		props?: {
-			[K in keyof WritableInstanceProperties<I>]?:
-				| WritableInstanceProperties<I>[K]
-				| State<WritableInstanceProperties<I>[K]>;
-		},
-		onChanged?: {
-			[K in InstancePropertyNames<I>]?: (newValue: I[K]) => void;
-		},
-		onEvent?: {
-			[K in InstanceEventNames<I>]?: I[K] extends RBXScriptSignal<infer C>
-				? (...args: Parameters<C>) => void
-				: never;
-		},
-		children?: ChildrenValue,
-	): I;
+	// Types
+	export { Animatable, Error, StateOrValue };
+	// Instances
+	export { Children, ChildrenValue, New, NewProperties, OnChange, OnEvent };
 }
 
 export = Fusion;
